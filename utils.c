52539efd580600e9ad2efe0e739336ed9b1e8616
Sebastian Ramacher <s.ramacher@gmx.at>
2010-12-22 17:52:58 +0100

    implement girara_get_home_directory

--- utils.c
+++ utils.c
@@ -1,6 +1,14 @@
 /* See LICENSE file for license and copyright information */
 
+#define _BSD_SOURCE
+#define _XOPEN_SOURCE 500
+
+#include <limits.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <pwd.h>
+#include <unistd.h>
 #include "girara-utils.h"
 
 gchar*
@@ -8,23 +16,89 @@ girara_fix_path(const gchar* path)
 {
   g_return_val_if_fail(path != NULL || !strlen(path), NULL);
 
+  gchar* rpath = NULL;
   if (path[0] == '~')
   {
-    gchar* home_path = girara_get_home_directory(NULL);
-    gchar* res = g_build_filename(home_path, path + 1, NULL);
+    int len = strlen(path);
+    gchar* user = NULL;
+    int idx = 1;
+    if (len > 1 && path[1] != '/')
+    {
+      while (path[idx] && path[idx] != '/')
+        ++idx;
+      user = g_strndup(path + 1, idx - 2);
+    }
+
+    gchar* home_path = girara_get_home_directory(user);
+    g_free(user);
+    if (!home_path)
+      return g_strdup(path);
+
+    rpath = g_build_filename(home_path, path + idx, NULL);
     g_free(home_path);
-    return res;
   }
   else
-    return g_strdup(path);
+    rpath = g_strdup(path);
+
+/*
+  size_t pm
+#ifdef PATH_MAX
+    = PATH_MAX;
+#else
+    = pathconf(path,_PC_PATH_MAX);
+  if(pm <= 0)
+    pm = 4096;
+#endif
+
+  gchar* file = g_malloc0(sizeof(gchar) * pm);
+  if (!file)
+  {
+    g_free(rpath);
+    return NULL;
+  }
+
+  gchar* res = realpath(rpath, file);
+  g_free(rpath);
+  if (!res)
+  {
+    g_free(file);
+    return NULL;
+  }
+
+  return file;
+  */
+  return rpath;
 }
 
 gchar*
-girara_get_home_directory(const gchar* user) {
-  g_return_val_if_fail(user == NULL, NULL);
+girara_get_home_directory(const gchar* user)
+{
+  if (!user || g_strcmp0(user, g_get_user_name()) == 0)
+  {
+    const gchar* homedir = g_getenv("HOME");
+    return g_strdup(homedir ? homedir : g_get_home_dir());
+  }
 
-  const gchar* homedir = g_getenv("HOME");
-  return g_strdup(homedir ? homedir : g_get_home_dir());
+  struct passwd pwd;
+  struct passwd* result;
+  int bufsize = sysconf(_SC_GETPW_R_SIZE_MAX);
+  if (bufsize < 0)
+    bufsize = 4096;
+
+  gchar* buffer = g_malloc0(sizeof(gchar) * bufsize);
+  if (!buffer)
+    return NULL;
+  
+  getpwnam_r(user, &pwd, buffer, bufsize, &result);
+  if (result == NULL)
+  {
+    g_free(buffer);
+    return NULL;
+  }
+
+  gchar* dir = g_strdup(pwd.pw_dir);
+  g_free(buffer);
+  return dir;
 }
 
 gchar*
