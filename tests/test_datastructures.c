80b67fb1255d0e5da5765b67039be41343a662d9
Sebastian Ramacher <s.ramacher@gmx.at>
2010-12-25 02:22:42 +0100

    more datastructure tests

--- tests/test_datastructures.c
+++ tests/test_datastructures.c
@@ -2,9 +2,21 @@
 #include <glib.h>
 #include "girara-datastructures.h"
 
-void test_datastructures_list()
+static unsigned int list_free_called = 0;
+
+static void
+list_free(void* data)
+{
+  g_assert_cmpuint(list_free_called, ==, 0);
+  g_assert_cmpuint((unsigned int) data, ==, 0xDEAD);
+  ++list_free_called;
+}
+
+void
+test_datastructures_list()
 {
   girara_list_t* list = girara_list_new();
+  g_assert_cmpuint(girara_list_size(list), ==, 0);
   for (int i = 0; i != 10; ++i)
     girara_list_append(list, (void*)i);
   g_assert_cmpuint(girara_list_size(list), ==, 10);
@@ -21,7 +33,19 @@ void test_datastructures_list()
   }
   girara_list_iterator_free(iter);
   girara_list_free(list);
+
+  list = girara_list_new();
+  girara_list_set_free_function(list, list_free);
+  girara_list_append(list, (void*)0xDEAD);
+  girara_list_free(list);
+  g_assert_cmpuint(list_free_called, ==, 1);
 }
 
-void test_datastructures_node()
-{}
+void
+test_datastructures_node()
+{
+  girara_tree_node_t* root = girara_node_new("root");
+  g_assert_cmpuint(girara_node_get_num_children(root), ==, 0);
+  g_assert_cmpstr((char*)girara_node_get_data(root), ==, "root");
+  girara_node_free(root);
+}
