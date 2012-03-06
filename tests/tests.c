2f9ab6f5be060c9f202d4af8a6a3c354fc268db2
Sebastian Ramacher <s.ramacher@gmx.at>
2012-03-06 11:38:08 +0100

    The test suite needs to exit with non-zero if a test fails.

--- tests/tests.c
+++ tests/tests.c
@@ -2,6 +2,7 @@
 
 #include <check.h>
 #include <gtk/gtk.h>
+#include <stdlib.h>
 
 Suite* suite_utils();
 Suite* suite_datastructures();
@@ -13,38 +14,43 @@ int main(int argc, char *argv[])
 {
   Suite* suite          = NULL;
   SRunner* suite_runner = NULL;
+  int number_failed     = 0;
 
   /* test utils */
   suite        = suite_utils();
   suite_runner = srunner_create(suite);
   srunner_run_all(suite_runner, CK_NORMAL);
+  number_failed += srunner_ntests_failed(suite_runner);
   srunner_free(suite_runner);
 
   /* test datastructures */
   suite        = suite_datastructures();
   suite_runner = srunner_create(suite);
   srunner_run_all(suite_runner, CK_NORMAL);
+  number_failed += srunner_ntests_failed(suite_runner);
   srunner_free(suite_runner);
 
   /* test settings */
   suite        = suite_settings();
   suite_runner = srunner_create(suite);
   srunner_run_all(suite_runner, CK_NORMAL);
+  number_failed += srunner_ntests_failed(suite_runner);
   srunner_free(suite_runner);
 
   /* test config */
   suite        = suite_config();
   suite_runner = srunner_create(suite);
   srunner_run_all(suite_runner, CK_NORMAL);
+  number_failed += srunner_ntests_failed(suite_runner);
   srunner_free(suite_runner);
-
 
   /* test session */
   gtk_init(&argc, &argv);
   suite        = suite_session();
   suite_runner = srunner_create(suite);
   srunner_run_all(suite_runner, CK_NORMAL);
+  number_failed += srunner_ntests_failed(suite_runner);
   srunner_free(suite_runner);
 
-  return 0;
+  return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
