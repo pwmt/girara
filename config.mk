80181de53d568b45d4cee28bdaebbfa793b40151
Moritz Lipp <mlq@pwmt.org>
2012-03-24 21:10:29 +0100

    Version 0.1.2

--- config.mk
+++ config.mk
@@ -2,7 +2,7 @@
 
 GIRARA_VERSION_MAJOR = 0
 GIRARA_VERSION_MINOR = 1
-GIRARA_VERSION_REV   = 1
+GIRARA_VERSION_REV   = 2
 VERSION = ${GIRARA_VERSION_MAJOR}.${GIRARA_VERSION_MINOR}.${GIRARA_VERSION_REV}
 # Rules for the SOMAJOR and SOMINOR.
 # Before a release check perform the following checks against the last release:
@@ -12,7 +12,7 @@ VERSION = ${GIRARA_VERSION_MAJOR}.${GIRARA_VERSION_MINOR}.${GIRARA_VERSION_REV}
 # 	bump SOMAJOR and set SOMINOR to 0.
 # * If a function has been added bump SOMINOR.
 SOMAJOR = 0
-SOMINOR = 0
+SOMINOR = 1
 SOVERSION = ${SOMAJOR}.${SOMINOR}
 
 # paths
