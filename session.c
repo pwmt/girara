pwmt/girara

commit a026b72b297b7d83f3ae65baa3bde5b8be601208

reorganize widgets from statusbar inputbar and friends
We group the status input notification and completion widgets into a new
bottom_box container.

Also, make sure, particularly in GTK3, that status, input and
notification bars get the same height.

Signed-off-by: Sebastian Ramacher <sebastian+dev@ramacher.at>

session.c
@@ -88,11 +88,13 @@ girara_session_create()
   /* create widgets */
 #if GTK_MAJOR_VERSION == 2
   session->gtk.box               = GTK_BOX(gtk_vbox_new(FALSE, 0));
+  session->gtk.bottom_box        = GTK_BOX(gtk_vbox_new(FALSE, 0));
   session->gtk.statusbar_entries = GTK_BOX(gtk_hbox_new(FALSE, 0));
   session->gtk.tabbar            = gtk_hbox_new(TRUE, 0);
   session->gtk.inputbar_box      = GTK_BOX(gtk_hbox_new(TRUE, 0));
 #else
   session->gtk.box               = GTK_BOX(gtk_box_new(GTK_ORIENTATION_VERTICAL, 0));
+  session->gtk.bottom_box        = GTK_BOX(gtk_box_new(GTK_ORIENTATION_VERTICAL, 0));
   session->gtk.statusbar_entries = GTK_BOX(gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0));
   session->gtk.tabbar            = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
   session->gtk.inputbar_box      = GTK_BOX(gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0));
@@ -216,14 +218,65 @@ girara_session_init(girara_session_t* session, const char* sessionname)
 
   /* notification area */
   gtk_container_add(GTK_CONTAINER(session->gtk.notification_area), GTK_WIDGET(session->gtk.notification_text));
-  gtk_misc_set_alignment(GTK_MISC(session->gtk.notification_text), 0.0, 0.0);
-  gtk_misc_set_padding(GTK_MISC(session->gtk.notification_text), 2, 2);
+  gtk_misc_set_alignment(GTK_MISC(session->gtk.notification_text), 0.0, 0.5);
   gtk_label_set_use_markup(GTK_LABEL(session->gtk.notification_text), TRUE);
 
   /* inputbar */
-  gtk_entry_set_inner_border(session->gtk.inputbar_entry, NULL);
   gtk_entry_set_has_frame(session->gtk.inputbar_entry, FALSE);
   gtk_editable_set_editable(GTK_EDITABLE(session->gtk.inputbar_entry), TRUE);
+
+  /* we want inputbar_entry the same height as notification_text and statusbar,
+     so that when inputbar_entry is hidden, the size of the bottom_box remains
+     the same. We need to get rid of the builtin padding in the GtkEntry
+     widget. */
+
+  guint ypadding = 2;     /* total amount of padding (top + bottom) */
+  guint leftpadding = 4;  /* left padding */
+
+#if (GTK_MAJOR_VERSION == 3)
+  /* gtk_entry_set_inner_border is deprecated since gtk 3.4 and does nothing. */
+  GtkCssProvider* provider = gtk_css_provider_new();
+  char css[256];
+  const char* css_pattern = "#bottom_box { padding:%dpx 0px %dpx %dpx; }";
+  sprintf(css, css_pattern, ypadding - ypadding/2, ypadding/2, leftpadding);
+
+  gtk_css_provider_load_from_data(provider, css, strlen(css), NULL);
+  GdkDisplay *display = gdk_display_get_default ();
+  GdkScreen *screen = gdk_display_get_default_screen (display);
+  gtk_style_context_add_provider_for_screen (screen,
+                                             GTK_STYLE_PROVIDER (provider),
+                                             GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+  g_object_unref (provider);
+
+  gtk_widget_set_name(session->gtk.inputbar_entry, "bottom_box");
+  gtk_widget_set_name(session->gtk.notification_text, "bottom_box");
+#else
+  // TODO: test this GTK2 block
+  GtkBorder inner_border = {
+      .left = leftpadding,
+      .right = 0,
+      .top = ypadding - ypadding/2,
+      .bottom = ypadding/2
+  };
+
+  gtk_entry_set_inner_border(session->gtk.inputbar_entry, &inner_border);
+
+  /* obtain the actual inputbar height */
+  /* TODO: for some reason does not match */
+  GtkRequisition req;
+  gtk_widget_size_request(GTK_WIDGET(session->gtk.inputbar_entry), &req);
+  /* have no idea where the extra 5 pixels come from. without this, the other
+     widgets get larger than the inputbar */
+  guint statusbar_height = req.height - 5;
+
+  /* force all widgets to have the same height as inputbar */
+  gtk_widget_set_size_request(GTK_WIDGET(session->gtk.inputbar_entry), -1, statusbar_height);
+  gtk_widget_set_size_request(GTK_WIDGET(session->gtk.statusbar_entries), -1, statusbar_height);
+  gtk_widget_set_size_request(GTK_WIDGET(session->gtk.notification_text), -1, statusbar_height);
+
+  /* set horizontal padding. same for statusbar entries in statusbar.c */
+  gtk_misc_set_padding(GTK_MISC(session->gtk.notification_text), leftpadding, 0);
+#endif
 
   session->signals.inputbar_key_pressed = g_signal_connect(
       G_OBJECT(session->gtk.inputbar_entry),
@@ -249,9 +302,17 @@ girara_session_init(girara_session_t* session, const char* sessionname)
   gtk_box_set_homogeneous(session->gtk.inputbar_box, FALSE);
   gtk_box_set_spacing(session->gtk.inputbar_box, 5);
 
+  /* inputbar box */
   gtk_box_pack_start(GTK_BOX(session->gtk.inputbar_box),  GTK_WIDGET(session->gtk.inputbar_dialog), FALSE, FALSE, 0);
   gtk_box_pack_start(GTK_BOX(session->gtk.inputbar_box),  GTK_WIDGET(session->gtk.inputbar_entry),  TRUE,  TRUE,  0);
   gtk_container_add(GTK_CONTAINER(session->gtk.inputbar), GTK_WIDGET(session->gtk.inputbar_box));
+
+  /* bottom box */
+  gtk_box_set_spacing(session->gtk.bottom_box, 0);
+
+  gtk_box_pack_end(GTK_BOX(session->gtk.bottom_box), GTK_WIDGET(session->gtk.inputbar), TRUE, TRUE, 0);
+  gtk_box_pack_end(GTK_BOX(session->gtk.bottom_box), GTK_WIDGET(session->gtk.notification_area), TRUE, TRUE, 0);
+  gtk_box_pack_end(GTK_BOX(session->gtk.bottom_box), GTK_WIDGET(session->gtk.statusbar), TRUE, TRUE, 0);
 
   /* tabs */
   gtk_notebook_set_show_border(session->gtk.tabs, FALSE);
@@ -260,9 +321,7 @@ girara_session_init(girara_session_t* session, const char* sessionname)
   /* packing */
   gtk_box_pack_start(session->gtk.box, GTK_WIDGET(session->gtk.tabbar),            FALSE, FALSE, 0);
   gtk_box_pack_start(session->gtk.box, GTK_WIDGET(session->gtk.view),              TRUE,  TRUE, 0);
-  gtk_box_pack_start(session->gtk.box, GTK_WIDGET(session->gtk.statusbar),         FALSE, FALSE, 0);
-  gtk_box_pack_start(session->gtk.box, GTK_WIDGET(session->gtk.notification_area), FALSE, FALSE, 0);
-  gtk_box_pack_end(  session->gtk.box, GTK_WIDGET(session->gtk.inputbar),          FALSE, FALSE, 0);
+  gtk_box_pack_end(session->gtk.box, GTK_WIDGET(session->gtk.bottom_box),          FALSE, FALSE, 0);
 
   /* parse color values */
   typedef struct color_setting_mapping_s

completion.c
@@ -380,7 +380,7 @@ girara_isc_completion(girara_session_t* session, girara_argument_t* argument, gi
 
     if (entries != NULL) {
       entries_current = (argument->n == GIRARA_NEXT) ? g_list_last(entries) : entries;
-      gtk_box_pack_start(session->gtk.box, GTK_WIDGET(session->gtk.results), FALSE, FALSE, 0);
+      gtk_box_pack_start(session->gtk.bottom_box, GTK_WIDGET(session->gtk.results), FALSE, FALSE, 0);
       gtk_widget_show(GTK_WIDGET(session->gtk.results));
     }
   }

session.h
@@ -27,6 +27,7 @@ struct girara_session_s
     GtkBox          *box; /**< A box that contains all widgets */
     GtkWidget       *view; /**< The view area of the applications widgets */
     GtkWidget       *viewport; /**< The viewport of view */
+    GtkBox          *bottom_box; /**< Box grouping input, status and notification */
     GtkWidget       *statusbar; /**< The statusbar */
     GtkBox          *statusbar_entries; /**< Statusbar entry box */
     GtkWidget       *notification_area; /**< The notification area */

statusbar.c
@@ -25,9 +25,16 @@ girara_statusbar_item_add(girara_session_t* session, bool expand, bool fill, boo
   gtk_widget_override_font(GTK_WIDGET(item->text),             session->style.font);
 
   /* set properties */
-  gtk_misc_set_alignment(GTK_MISC(item->text),     left ? 0.0 : 1.0, 0.0);
-  gtk_misc_set_padding(GTK_MISC(item->text),       2, 4);
+  gtk_misc_set_alignment(GTK_MISC(item->text),     left ? 0.0 : 1.0, 0.5);
   gtk_label_set_use_markup(item->text,             TRUE);
+
+#if (GTK_MAJOR_VERSION == 3)
+  /* add name so it uses a custom style */
+  gtk_widget_set_name(item->text, "bottom_box");
+#else
+  /* set padding */
+  gtk_misc_set_padding(GTK_MISC(item->text), 4, 0);
+#endif
 
   if (callback != NULL) {
     g_signal_connect(G_OBJECT(item->box), "button-press-event", G_CALLBACK(callback), session);
@@ -85,4 +92,3 @@ girara_statusbar_set_background(girara_session_t* session, const char* color)
 
   return true;
 }
-

commit e67efb4566c76e4cb362c4373401cf1d1fcadf95

Fix notification bar height on GTK2
Fixes regression introduced in commit a026b72b297b7d83 which rendered
the notification bar of fixed height, truncating multiline messages.

This should fix Issue 338.

Signed-off-by: Sebastian Ramacher <sebastian+dev@ramacher.at>

session.c
@@ -247,15 +247,15 @@ girara_session_init(girara_session_t* session, const char* sessionname)
      widget. */
 
   guint ypadding = 2;         /* total amount of padding (top + bottom) */
-  guint leftpadding = 4;      /* left padding */
+  guint xpadding = 8;         /* total amount of padding (left + right) */
   girara_setting_get(session, "statusbar-padding", &ypadding);
 
 #if (GTK_MAJOR_VERSION == 3)
   /* gtk_entry_set_inner_border is deprecated since gtk 3.4 and does nothing. */
   GtkCssProvider* provider = gtk_css_provider_new();
 
-  static const char CSS_PATTERN[] = "#bottom_box { border-style: none; margin: 0px 0px 0px 0px; padding:%dpx 0px %dpx %dpx; }";
-  char* css = g_strdup_printf(CSS_PATTERN, ypadding - ypadding/2, ypadding/2, leftpadding);
+  static const char CSS_PATTERN[] = "#bottom_box { border-style: none; margin: 0px 0px 0px 0px; padding:%dpx %dpx %dpx %dpx; }";
+  char* css = g_strdup_printf(CSS_PATTERN, ypadding - ypadding/2, xpadding/2, ypadding/2, xpadding - xpadding/2);
   gtk_css_provider_load_from_data(provider, css, strlen(css), NULL);
   g_free(css);
 
@@ -270,29 +270,25 @@ girara_session_init(girara_session_t* session, const char* sessionname)
   gtk_widget_set_name(GTK_WIDGET(session->gtk.notification_text), "bottom_box");
 #else
   GtkBorder inner_border = {
-      .left = leftpadding,
-      .right = 0,
+      .left = xpadding - xpadding/2,
+      .right = xpadding/2,
       .top = ypadding - ypadding/2,
       .bottom = ypadding/2
   };
 
+  /* set inner borders for inputbar and notification_text */
   gtk_entry_set_inner_border(session->gtk.inputbar_entry, &inner_border);
+  gtk_misc_set_padding(GTK_MISC(session->gtk.notification_text), xpadding/2, ypadding/2);
 
   /* obtain the actual inputbar height */
-  /* TODO: for some reason does not match */
   GtkRequisition req;
   gtk_widget_size_request(GTK_WIDGET(session->gtk.inputbar_entry), &req);
-  /* have no idea where the extra 5 pixels come from. without this, the other
-     widgets get larger than the inputbar */
+  /* have no idea where the extra 5 pixels come from. without this, the statusbar
+     gets larger than the inputbar */
   guint statusbar_height = req.height - 5;
 
-  /* force all widgets to have the same height as inputbar */
-  gtk_widget_set_size_request(GTK_WIDGET(session->gtk.inputbar_entry), -1, statusbar_height);
+  /* force statusbar to have same height as inputbar */
   gtk_widget_set_size_request(GTK_WIDGET(session->gtk.statusbar_entries), -1, statusbar_height);
-  gtk_widget_set_size_request(GTK_WIDGET(session->gtk.notification_text), -1, statusbar_height);
-
-  /* set horizontal padding. same for statusbar entries in statusbar.c */
-  gtk_misc_set_padding(GTK_MISC(session->gtk.notification_text), leftpadding, 0);
 #endif
 
   session->signals.inputbar_key_pressed = g_signal_connect(