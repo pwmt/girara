c53aeff6e2f60fc554ef27ba2535b0f6973aa19d
Sebastian Ramacher <sebastian+dev@ramacher.at>
2013-11-04 20:53:02 +0100

    Add a Bugs section to README
    Signed-off-by: Sebastian Ramacher <sebastian+dev@ramacher.at>

--- tests/config.mk
+++ tests/config.mk
@@ -1,6 +1,6 @@
 # See LICENSE file for license and copyright information
 
 CHECK_INC ?= $(shell pkg-config --cflags check)
-CHECK_LIB ?= $(shell pkg-config --libs check)
+CHECK_LIB ?= $(shell pkg-config --libs check) -pthread
 
 LIBS += ${CHECK_LIB}
