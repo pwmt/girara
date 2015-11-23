c6c1e8a71f8fe8a839f1e95d4e3bc3554735eb2a
Sebastian Ramacher <sebastian+dev@ramacher.at>
2015-11-23 19:04:55 +0100

    Require GTK+ 3.4
    Signed-off-by: Sebastian Ramacher <sebastian+dev@ramacher.at>

--- girara/session.c
+++ girara/session.c
@@ -268,9 +268,7 @@ girara_session_create()
   gtk_box_set_homogeneous(session->gtk.inputbar_box, TRUE);
   session->gtk.view              = gtk_scrolled_window_new(NULL, NULL);
   session->gtk.viewport          = gtk_viewport_new(NULL, NULL);
-#if GTK_CHECK_VERSION(3, 4, 0)
   gtk_widget_add_events(session->gtk.viewport, GDK_SCROLL_MASK);
-#endif
   session->gtk.statusbar         = gtk_event_box_new();
   session->gtk.notification_area = gtk_event_box_new();
   session->gtk.notification_text = gtk_label_new(NULL);
@@ -289,13 +287,11 @@ girara_session_init(girara_session_t* session, const char* sessionname)
     return false;
   }
 
-#if GTK_CHECK_VERSION(3, 4, 0)
   bool smooth_scroll = false;
   girara_setting_get(session, "smooth-scroll", &smooth_scroll);
   if (smooth_scroll) {
     gtk_widget_add_events(session->gtk.viewport, GDK_SMOOTH_SCROLL_MASK);
   }
-#endif
 
   session->private_data->session_name = g_strdup(
       (sessionname == NULL) ? "girara" : sessionname);
