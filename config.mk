dbad6010cec78cb2449aec088d80c2d969087350
Sebastian Ramacher <sebastian+dev@ramacher.at>
2017-06-26 18:44:17 +0200

    Bump SOMAJOR

--- config.mk
+++ config.mk
@@ -12,7 +12,7 @@ VERSION = ${GIRARA_VERSION_MAJOR}.${GIRARA_VERSION_MINOR}.${GIRARA_VERSION_REV}
 # * If any of the exported datastructures have changed in a incompatible way
 #   bump SOMAJOR and set SOMINOR to 0.
 # * If a function has been added bump SOMINOR.
-SOMAJOR = 2
+SOMAJOR = 3
 SOMINOR = 0
 SOVERSION = ${SOMAJOR}.${SOMINOR}
 
