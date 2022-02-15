dfef73e6d8fa3b6fd85af652a6dd7d7017c23a9d
JezerM <amyuki4@icloud.com>
2022-02-15 09:29:28 -0600

    Move bottom_box to main box instead of overlay

--- girara/session.c
+++ girara/session.c
@@ -539,11 +539,12 @@ girara_session_init(girara_session_t* session, const char* sessionname)
 
   /* box */
   gtk_container_add(GTK_CONTAINER(session->private_data->gtk.overlay), GTK_WIDGET(session->gtk.box));
-  /* overlay */
+  /* bottom_box */
   g_object_set(session->private_data->gtk.bottom_box, "halign", GTK_ALIGN_FILL, NULL);
   g_object_set(session->private_data->gtk.bottom_box, "valign", GTK_ALIGN_END, NULL);
 
-  gtk_overlay_add_overlay(GTK_OVERLAY(session->private_data->gtk.overlay), GTK_WIDGET(session->private_data->gtk.bottom_box));
+  gtk_container_add(GTK_CONTAINER(session->gtk.box), GTK_WIDGET(session->private_data->gtk.bottom_box));
+
   gtk_container_add(GTK_CONTAINER(session->gtk.window), GTK_WIDGET(session->private_data->gtk.overlay));
 
   /* statusbar */
